96216b656291cce3aaef0ef738b4cc016aa757f0
lequocanh20192684 <anh.lq192684@sis.hust.edu.vn>
2020-12-07 01:09:59 +0700

    Initial Commit

---
+++ bài 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bài 1
+//
+//  Created by Anh Lee on 07/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
